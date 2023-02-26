19bc738474230b9eec62ea82cf9e20b0bfa99a1f
DaniSRV <nata78s@gmail.com>
2023-02-26 05:27:21 +0300

    new files: ui, fonts, buttons, forms

--- client/js/app.js
+++ client/js/app.js
@@ -1,7 +1,28 @@
-const btn = document.querySelector('.btn-selection')
-const ul = document.querySelector('.list-city')
-let buttonFunc = () => {
-btn.classList.toggle('clicked')
-ul.classList.toggle('show')
-}
-btn.addEventListener('click', buttonFunc)
+const btns = document.querySelectorAll('.btn-toggle')
+btns.forEach(btn => {
+  console.log(btn)  
+  let buttonFunc = () => {
+    btn.classList.toggle('clicked')
+  }
+  btn.addEventListener('click', buttonFunc)
+})
+console.log(btns)
+
+const heart = document.querySelector('.btn-marker')
+let heartFunc = () => {
+  const span = heart.querySelector('span')
+  heart.classList.toggle('active')
+  if (heart.classList.contains('active')) {
+    span.textContent = 'Добавлено'
+  } else {
+    span.textContent ='В закладки'
+  }
+}  
+heart.addEventListener('click', heartFunc)
+
+const tiles = document.querySelector('.btn-tiles')
+let tilesFunc =() => {
+    tiles.classList.toggle('active')
+  }
+tiles.addEventListener('click', tilesFunc)
+
